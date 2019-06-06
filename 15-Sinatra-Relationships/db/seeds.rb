905431c3d810c21292471d055993410870b61f06
Eric Kim <hyeokjungkim@gmail.com>
2019-06-06 10:56:55 -0400

    adds lecture

---
+++ 15-Sinatra-Relationships/db/seeds.rb
@@ -0,0 +1,8 @@
+students = ["Avitosh Totaram", "Carla Stickler", "Chloe Liu", "Daniela Sandoval", "Danielle O'Neill", "Duong Nguyen",
+"Fanqiang Meng", "Hannah Ajayi", "Joe Jamsky", "Joseph G Syverson", "Joshua Suskin",
+"Kenton Archer", "Kevin Wang", "Lorena Ramirez-Lopez", "Otha Hernandez", "Rachel Rath",
+"Stefanos Ugbit", "Tanuka Das", "Tess Neau", "Tom MacLean", "Toni Lamont", "Yonas Fesehatsion", "Gregory Dwyer", "Eric Kim"]
+
+students.shuffle.each do |student_name|
+  Student.create(name: student_name, age: rand(18), slogan: Faker::Quote.famous_last_words)
+end
