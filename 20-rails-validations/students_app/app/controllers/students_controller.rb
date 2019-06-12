cdf5c3a3f3b657bd8feb0c1ae3db5aace175008e
Eric Kim <hyeokjungkim@gmail.com>
2019-06-12 12:55:34 -0400

    adds lecture

--- 20-rails-validations/students_app/app/controllers/students_controller.rb
+++ 20-rails-validations/students_app/app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < ApplicationController
+  before_action :find_student, only: [:show, :edit, :update, :delete]
+
   def index
     @students = Student.all
     # render :index
   end
 
   def show
-    @student = Student.find(params[:id])
     @student_lab = StudentLab.new
     @labs = Lab.all
   end
@@ -15,43 +16,54 @@ class StudentsController < ApplicationController
   end
 
   def create
-    student = Student.create(student_params)
-
-    redirect_to student
-    # "/students/#{@student.id}"
+    @student = Student.create(student_params)
+    if @student.valid?
+      redirect_to student
+    else
+      flash[:errors] = @student.errors.full_messages
+      # render :new
+      redirect_to new_student_path
+    end
   end
 
   def edit
-    @student = Student.find(params[:id])
+    # BEFORE ACTION IS TAKING CARE OF EVERYTHING WE GOOD
   end
 
   def update
-    student = Student.find(params[:id])
-    student.update(student_params)
+    @student.update(student_params)
+    if @student.valid?
+      redirect_to @student
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to edit_student_path(@student)
+    end
 
-    redirect_to student
     # "/students/#{@student.id}"
   end
 
   def destroy
-    student = Student.find(params[:id])
-    student.destroy
+    @student.destroy
 
     redirect_to students_path
   end
 
   # EXTRA
   def create_labs
-    @student = Student.find(params[:id])
     student_lab = StudentLab.create(lab_id: params[:student_lab][:lab_id], student_id: @student.id)
     redirect_to @student
   end
   # EXTRA
-  
+
   private
 
   def student_params
     params.require(:student).permit(:name, :age, :favorite_food)
   end
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
 
 end
--- 20-rails-validations/students_app/app/models/student.rb
+++ 20-rails-validations/students_app/app/models/student.rb
@@ -2,8 +2,21 @@ class Student < ApplicationRecord
   has_many :student_labs
   has_many :labs, through: :student_labs
 
+  validates :name, presence: true, uniqueness: true
+  # validates :name, uniqueness: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+
+  validates :favorite_food, presence: true
+  validate :thou_shalt_not_be_called_eric
+
   def updog
     name.upcase
+  end
+
+  def thou_shalt_not_be_called_eric
+    if self.name && self.name.downcase == "eric"
+      self.errors.add(:name, "shall not be called Eric")
+    end
   end
 
 end
