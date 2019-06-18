d99a94ed6b2b20898f03fee7e2faa2392c82f7b5
Eric Kim <hyeokjungkim@gmail.com>
2019-06-18 13:42:50 -0400

    adds lecture

--- 22-rails-auth/app/controllers/login_controller.rb
+++ 22-rails-auth/app/controllers/login_controller.rb
@@ -1,6 +1,6 @@
 class LoginController < ApplicationController
+  
   def new
-
   end
 
   def create
--- 22-rails-auth/app/controllers/users_controller.rb
+++ 22-rails-auth/app/controllers/users_controller.rb
@@ -19,22 +19,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :username, :password)
   end
 
-  # def edit_params
-  #   params.require(:user).permit(:name)
-  # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
