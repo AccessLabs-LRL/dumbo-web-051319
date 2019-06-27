1dce12bce24b5dd96a3d99fe28f49df724c83d13
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-06-27 11:06:22 -0400

    fetch cr

--- 27-fetch-crud/index.js
+++ 27-fetch-crud/index.js
@@ -1,18 +1,21 @@
 
+function slapOneToDoOnTheDOM(ol, toDoItem){
+  const li = document.createElement("li");
+  // console.log("list-item-" + toDoItem.id)
+  li.id = "list-item-" + toDoItem.id
+  li.class = "item"
+  // console.log(toDoItem)
+  li.innerHTML = `
+    <input id="to-do-item-${ toDoItem.id }" type="checkbox" ${ toDoItem.done ? "checked" : "" } />
+    <label for="to-do-item-${ toDoItem.id }" class="js-title middle aligned content">${ toDoItem.title }</label>
+  `
+  ol.append(li)
+}
 
 function slapToDosOnTheDOM(listItems){
   const ol = document.querySelector("#to-do-items")
-  listItems.forEach(function(toDoItem){
-    const li = document.createElement("li");
-    // console.log("list-item-" + toDoItem.id)
-    li.id = "list-item-" + toDoItem.id
-    li.class = "item"
-    console.log(toDoItem)
-    li.innerHTML = `
-      <input id="to-do-item-${ toDoItem.id }" type="checkbox" ${ toDoItem.done ? "checked" : "" } />
-      <label for="to-do-item-${ toDoItem.id }" class="js-title middle aligned content">${ toDoItem.title }</label>
-    `
-    ol.append(li)
+  listItems.forEach(function(listItem){ 
+    slapOneToDoOnTheDOM(ol, listItem)
   })
 }
 
@@ -20,4 +23,35 @@ document.addEventListener("DOMContentLoaded", function(){
   const promise = fetch("http://localhost:3000/to-do-items")
   promise.then(response => response.json())
          .then(slapToDosOnTheDOM)
+
+
+  const form = document.querySelector("#new-to-do")
+  const ol = document.querySelector("#to-do-items")
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    fetch("http://localhost:3000/to-do-items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "title": form.title.value,
+        "done": false
+      })
+    })
+      .then(res => res.json())
+      .then(data => slapOneToDoOnTheDOM(ol, data))
+  })
 }) 
+
+
+
+
+
+
+
+
+
+
+
+
