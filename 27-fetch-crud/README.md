545f62b6ed2e1b0f9a88ac5b5737b4f50655dcfd
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-06-27 10:44:20 -0400

    sfdjkhsdfjkhds

---
+++ 27-fetch-crud/README.md
@@ -0,0 +1,26 @@
+# Key Questions
+* What is REST again and why do we still care about it?
+* What is json-server and how can it help us prototype a RESTful API?
+* How can I work with an API that responds with a list of JSON objects?
+* How can I create data in a RESTful API using fetch?
+
+1. When X event happens
+2. Make Y fetch happen
+3. Slap Z on the DOM!
+
+Swampcast
+
+
+
+## Read
+1. When DOMContentLoaded happens
+2. Make GET /to-do-items fetch happen
+3. Slap a bunch of LI's on the DOM!
+
+
+## Create
+1. When sumbit of the new form happens
+2. Make POST /to-do-items fetch happen
+3. Slap one single LI on the DOM!
+
+
--- 27-fetch-crud/index.js
+++ 27-fetch-crud/index.js
@@ -1,53 +1,23 @@
 
 
-function slapToDoOnTheDOM(toDoItem){
-  const li = document.createElement("li")
-  li.id = "list-item-" + toDoItem.id
-  li.className = "item"
-  li.innerHTML = `
-  <input id="to-do-item-${ toDoItem.id }" type="checkbox" ${ toDoItem.done ? "checked" : "" }/>
-  <label for="to-do-item-${ toDoItem.id }" class="js-title middle aligned content">${ toDoItem.title }</label>`
-  // li.querySelector(".js-delete-button").addEventListener("click", destroyToDo)
-  // li.querySelector(".js-edit-button").addEventListener("click", editToDo)
-  return li
-}
-
-function slapToDosOnTheDOM(toDoListElement, data) {
-  toDoListElement.append(...data.map(slapToDoOnTheDOM))
-}
-
-function fetchToDos(element) {
-  return fetch("http://localhost:3000/to-do-items")
-    .then(res => res.json())
-}
-
-function createToDo(event, toDoListElement){
-  event.preventDefault();
-  const title = event.target.title.value;
-  if (!title.replace(/\s/g, "").length) {
-    event.target.classList.add("error")
-    return
-  }
-  event.target.reset()
-  event.target.classList.remove("error")
-  fetch("http://localhost:3000/to-do-items", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      title: title
-    })
-  }).then(response => response.json())
-  .then(data => {
-    toDoListElement.append(slapToDoOnTheDOM(data))
+function slapToDosOnTheDOM(listItems){
+  const ol = document.querySelector("#to-do-items")
+  listItems.forEach(function(toDoItem){
+    const li = document.createElement("li");
+    // console.log("list-item-" + toDoItem.id)
+    li.id = "list-item-" + toDoItem.id
+    li.class = "item"
+    console.log(toDoItem)
+    li.innerHTML = `
+      <input id="to-do-item-${ toDoItem.id }" type="checkbox" ${ toDoItem.done ? "checked" : "" } />
+      <label for="to-do-item-${ toDoItem.id }" class="js-title middle aligned content">${ toDoItem.title }</label>
+    `
+    ol.append(li)
   })
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-  const toDoListElement = document.getElementById("to-do-items");
-  fetchToDos().then(data => slapToDosOnTheDOM(toDoListElement, data))
-  const newForm = document.getElementById("new-to-do")
-  newForm.addEventListener("submit", event => createToDo(event, toDoListElement))
-})
+  const promise = fetch("http://localhost:3000/to-do-items")
+  promise.then(response => response.json())
+         .then(slapToDosOnTheDOM)
+}) 
