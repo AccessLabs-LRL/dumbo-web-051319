55504c9ab39d1573339596d67c3e19305d42a67a
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-07-09 10:58:52 -0400

    oojs

---
+++ 29-oojs/index.js
@@ -0,0 +1,74 @@
+class Person {
+
+  constructor(name = "Tess", fullName){
+    this.name = name
+    this.fullName = fullName
+  }
+
+  introduce() {
+    return `HEY, it's ${ this.fullName }!`
+  }
+
+}
+
+class Instructor extends Person {
+
+  talkAboutOO(){
+    return "bablabsl"
+  }
+
+}
+
+graham = new Instructor("Graham", "Graham Troyer-Joy")
+ariana = new Person("Ariana", "Ariana Grande")
+
+
+// function Instructor(fullName){
+//   this.fullName = fullName
+// }
+
+// Instructor.prototype.introduce = function(){
+//   return `whatever ${ this.fullName }`
+// }
+
+
+// const graham = new Instructor("Graham Troyer-Joy")
+// graham.introduce();
+
+
+
+// function introduce(){
+//   return `HEY, it's ${ this.fullName }!`
+// }
+
+// const mazen = {
+//   fullName: "Mazen Al Swar",
+//   name: "Mazen",
+//   introduce: introduce
+// }
+
+
+// const graham = {
+//   legalName: "Graham Aden Troyer-Joy",
+//   name: "Graham",
+//   introduce: introduce
+// }
+
+// const greg = {
+//   fullName: "Greg Dwyer",
+//   name: "Greg",
+//   introduce: introduce
+// }
+
+
+// console.log(mazen.introduce())
+// console.log(graham.introduce())
+// console.log(greg.introduce())
+
+// console.log(mazen.introduce === graham.introduce)
+// console.log(mazen.introduce() === graham.introduce())
+
+
+
+
+
